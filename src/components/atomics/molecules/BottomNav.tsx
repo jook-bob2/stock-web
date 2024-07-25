c62e3a41953acef8b4f51c6bc6040068e604bf40
jook-bob2 <qwefk123@naver.com>
2024-07-25 17:55:26 +0900

    타입오류 수정

--- src/components/atomics/molecules/BottomNav.tsx
+++ src/components/atomics/molecules/BottomNav.tsx
@@ -1,9 +1,8 @@
-import { bottomNav } from '@/components/layouts/mainLayout.css';
 import React from 'react';
 
 const BottomNav: React.FC = () => {
   return (
-    <nav className={bottomNav}>
+    <nav>
       <span>홈</span>
       <span>종목목록</span>
       <span>설정</span>
